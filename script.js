eef57226159df6a588c9220588d2f340e24001c2
ehsanbakhsh <ehsanbakhsh9499815@gmail.com>
2022-02-08 00:40:00 +0500

    updated files index.html and script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var alertBtn = document.getElementById("alertBtn");
 var clgBtn = document.getElementById("printConsole");
 var consoleWarning = document.getElementById("consoleWarning");
 var consoleError = document.getElementById("consoleError");
+var variableStore = document.getElementById("varStore");
 
 
 // add Event Listeners
@@ -12,6 +13,7 @@ alertBtn.addEventListener("click" , btnAlert);
 clgBtn.addEventListener('click' , printInConsole);
 consoleWarning.addEventListener('click' , consoleWarn);
 consoleError.addEventListener('click' , errorConsole);
+variableStore.addEventListener('click' , storedVar);
 
 
 // Event Functions
@@ -56,3 +58,20 @@ function errorConsole(){
 
     console.error(error);
 }
+
+
+// storedVar
+
+// function storedVar(){
+//     var firstVar = parseInt(prompt("Enter Your First Number.I'll Sum it With Second Number"));
+//     if (firstVar === NaN) {
+//         alert("Add some Value Please");
+//     }
+
+//     var secondVar = parseInt(prompt("Enter Your First Number.I'll Sum it With Second Number"));
+//     if (secondVar === NaN) {
+//         alert("Add some Value Please");
+//     }
+
+//     console.log(firstVar + secondVar);
+// }
